e890d02b1a3432567d7e4708da7a5471349b294d
tim <tim@timschupp.de>
2023-05-11 21:40:31 +0200

    integrated basic native / web switching technology

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,38 +1,25 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { handleStreamedProps, getCookiesAsObject, fetchDataOrLoadCache } from "../utils/tools";
 
 
-export const getCookiesAsObject = () => {
-  // stolen: https://stackoverflow.com/a/64472572
-  return Object.fromEntries(
-    document.cookie
-      .split('; ')
-      .map(v => v.split(/=(.*)/s).map(decodeURIComponent)),
-  );
-};
+
 
 export const getServerSideProps = async ({req} : {req: any}) => {
   if (req.method == "POST") {
-    const streamPromise = new Promise( ( resolve, reject ) => {
-        let body = ''
-        req.on('data', (chunk : any) => {
-          body += chunk
-        })
-        req.on('end', () => {
-          console.log(body);
-          resolve(body)
-        });
-    } );
-    const res = await streamPromise;
-    if (typeof res !== "string") throw new Error("Not a string")
+    const res = handleStreamedProps({req})
     return { props: { data: JSON.parse(res) } };
   }
   return { props: {} };
 };
 
-export default function Index({ state, updateTheme }): JSX.Element {
+
+export default function Index({ state, setState, updateTheme }): JSX.Element {
   //hello alter
   console.log("STATE", state);
+  fetchDataOrLoadCache({ state, setState })
 
-  return (<h1><button className="btn">Hello daisyUI</button></h1>);
+  return (<h1>
+    <button className="btn">Hello daisyUI</button>
+    <button className="btn btn-primary">data: {JSON.stringify(state)}</button>
+    </h1>);
 }
