58148d4b3dc890984dab7426dbc3ec43e075275c
tim <tim@timschupp.de>
2023-05-11 21:43:46 +0200

    simple tools, e.g.: generating default development admin user

---
+++ back/core/tools.py
@@ -0,0 +1,14 @@
+from core.models import User
+import json
+from django.conf import settings
+from random import randint
+
+
+def get_or_create_base_admin():
+    user = User.objects.filter(username=settings.BASE_ADMIN_USERNAME)
+    if not user.exists():
+        user = User.objects.create_superuser(
+            username=settings.BASE_ADMIN_USERNAME,
+            password=settings.BASE_ADMIN_USER_PASSWORD
+        )
+    return user
