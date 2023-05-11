b5f8cabb6a4bfef51fd84f066bbc5a59fd5286f7
tim <tim@timschupp.de>
2023-05-11 16:43:00 +0200

    base app setup reorganized moved back to manage.py file and added some more development requirements

--- back/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import django
-from django.core.management import execute_from_command_line
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
-    django.setup()
-    execute_from_command_line(sys.argv)
