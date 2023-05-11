0ab60062ad4090ea5db026ef062283e99142bd29
tim <tim@timschupp.de>
2023-05-11 21:42:09 +0200

    super basic user data implementation

--- back/core/api/user_data.py
+++ back/core/api/user_data.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from drf_spectacular.utils import extend_schema
 from dataclasses import dataclass
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes, authentication_classes, throttle_classes
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework import status
@@ -32,6 +33,7 @@ def get_user_data(user):
 )
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def request_user_data(request):
 
     return Response(get_user_data(request.user), status=status.HTTP_200_OK)
