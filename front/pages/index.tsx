25acb6a78cc72b77ba97e922ddffaaa5f279c8d6
tim <tim@timschupp.de>
2023-05-18 05:57:46 +0200

    simple navigation bar

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -16,6 +16,39 @@ export const NOgetServerSideProps = async ({req} : {req: any}) => {
   return { props: { dataLog: {pulled: false}} };
 };
 
+const NavBar = ({children}) => {
+  return (<div className="drawer">
+  <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+  <div className="drawer-content flex flex-col">
+    <div className="w-full navbar bg-base-300">
+      <div className="flex-none lg:hidden">
+        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+      </div> 
+      <div className="flex-1 px-2 mx-2">Navbar Title</div>
+      <div className="flex-none hidden lg:block">
+        <ul className="menu menu-horizontal">
+          <li><a>Navbar Item 1</a></li>
+          <li><a>Navbar Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+    Content
+    {children}
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 bg-base-100">
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+      
+    </ul>
+    
+  </div>
+</div>)
+}
+
 
 export default function Index({ pageState, setPageState, updateTheme }): JSX.Element {
   const router = useRouter();
@@ -39,6 +72,11 @@ export default function Index({ pageState, setPageState, updateTheme }): JSX.Ele
   console.log("STATE", JSON.stringify(state));
 
   return (<>
+    <NavBar>
+      <div style={{scrollBehavior: 'smooth', scrollPaddingTop: '5rem'}} className='content-center'>
+        <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xl" ></textarea>
+      </div>
+    </NavBar>
   <h1>
     <button className="btn" onClick={() => {
       console.log("WAS GEHT DENN SO");
