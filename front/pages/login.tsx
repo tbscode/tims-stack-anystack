9ff974ee5a2c996566b2b2b3e022c036ddf6164d
tim <tim@timschupp.de>
2023-05-18 03:32:41 +0200

    cleaned-up login page added basic connection banner also there

--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -1,18 +1,12 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import { Inter } from 'next/font/google'
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 import { useState, useEffect } from 'react';
+import { getCookiesAsObject, getEnv } from '@/utils/tools';
+import { ConnectionBanner, connectionStateAndUserData } from "../components/connection-banner";
 
-
-export const getCookiesAsObject = () => {
-  // stolen: https://stackoverflow.com/a/64472572
-  return Object.fromEntries(
-    document.cookie
-      .split('; ')
-      .map(v => v.split(/=(.*)/s).map(decodeURIComponent)),
-  );
-};
 
 export const NOgetServerSideProps = async ({req} : {req: any}) => {
   if (req.method == "POST") {
@@ -33,66 +27,48 @@ export const NOgetServerSideProps = async ({req} : {req: any}) => {
   return { props: {} };
 };
 
-const fetchDataOrLoadCache = ({ state, setStateData }) => {
- /**
-  *  There are multiple different env scenarios:
-  *  1. this is deployed as a web-app and the {state} is already automaticly populated when the page is first rendered
-  *  2. this is deployed as a mobile application and the {state} is not initally populated but needs to be fetched from the server
-  *  3. this is a mobile application but is not surrently connected to the internet, we should try to load data from local storage and display an 'offline' banner
-  * */ 
-  if(typeof window === 'undefined'){
-    // then we are still on serverside   
-    return
-  }
-  if (!Capacitor.isNativePlatform()) {
-    fetch('/api/user_data', {
-      method: 'GET',
-      headers: {
-        'X-CSRFToken': getCookiesAsObject().csrftoken,
-        'Content-Type': 'application/json',
-      },
-    }).then((res) => {
-      if(res.ok){
-        // the populate the state
-        // TODO: handle json parse errors
-        res.json().then((data) => {
-          setStateData(s => ({...s, device: {...s.device, isCapacitorNative: true},data: data}))
-        })
-      }else{
-        // TODO: if this is some sort of timeout error we should display a 'offline' banner
-        // On other erros ofcourse we should display a 'something went wrong' banner
-        console.log("RES NOT OK", res)
-        if(res.status == 401 || res.status == 403){
-          // then we are not logged in
-        }
-      }
-    })
-
-  }else{
-    // web app, then the state should be automaticly populated
-  }
-}
-
-export default function Index({ state, setStateData, updateTheme }): JSX.Element {
+export default function Index({ pageState, setPageState, updateTheme }): JSX.Element {
   //hello alter
-  console.log("STATE", state);
+  const router = useRouter();
   
   const [loginData, setLoginData] = useState({
     username: '',
     password: ''
   })
+
+  // TODO: setup some logig to initalize redux store with data from server
+  const [state, setState] = useState(pageState);
+  console.log("STATE", state);
+  
+  useEffect(() => {
+    const connectionState = connectionStateAndUserData({state}).then((connectionState) => {
+      console.log("CONNECTION STATE", connectionState);
+      if(connectionState.state === "unauthenticated"){
+        //router.push("/login");
+      }else{
+        console.log("SETTING PAGE STATE", connectionState);
+        setPageState(s => ({...s, ...connectionState}));
+        setState(s => ({...s, ...connectionState}));
+        router.push("/");
+      }
+  
+    })
+  }, []);
   
   const loginRequest = () => {
-    fetch('/api/login', {
+    fetch(`${getEnv().serverUrl}/api/login`, {
       method: 'POST',
       headers: {
         'X-CSRFToken': getCookiesAsObject().csrftoken,
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
+        Accept: 'application/json, text/html, */*'
       },
       body: JSON.stringify(loginData)
     }).then((res) => {
+      console.log("RES LOGIN", res)
       if(res.ok){
-        window.location.href = "/";
+        console.log("REDIRECTING")
+        router.push("/")
       }
     })
   }
@@ -106,5 +82,6 @@ export default function Index({ state, setStateData, updateTheme }): JSX.Element
       setLoginData(s => ({...s, password: e.target.value})) 
     }} />
     <button className="btn" onClick={loginRequest}>Login</button>
+    <ConnectionBanner state={state} />
   </>);
 }
