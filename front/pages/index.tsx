42df80b3ef5b9403f45dd0493b1b253aea00d40f
tim <tim@timschupp.de>
2023-05-18 03:33:13 +0200

    integrated new connection banner and state management logic

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,25 +1,56 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import { handleStreamedProps, getCookiesAsObject, fetchDataOrLoadCache } from "../utils/tools";
+import React, { useState, useEffect } from "react";
+import { ConnectionBanner, connectionStateAndUserData } from "../components/connection-banner";
 
 
 
 
-export const getServerSideProps = async ({req} : {req: any}) => {
+export const NOgetServerSideProps = async ({req} : {req: any}) => {
   if (req.method == "POST") {
-    const res = handleStreamedProps({req})
-    return { props: { data: JSON.parse(res) } };
+    const res = await handleStreamedProps({req})
+    console.log("RES", res)
+    return { props: { data: JSON.parse(res), dataLog: {pulled: true} } };
   }
-  return { props: {} };
+  return { props: { dataLog: {pulled: false}} };
 };
 
 
-export default function Index({ state, setState, updateTheme }): JSX.Element {
-  //hello alter
-  console.log("STATE", state);
-  fetchDataOrLoadCache({ state, setState })
+export default function Index({ pageState, setPageState, updateTheme }): JSX.Element {
+  const router = useRouter();
+  // TODO: setup some logig to initalize redux store with data from server
+  const [state, setState] = useState(pageState);
+  
+  useEffect(() => {
+    const connectionState = connectionStateAndUserData({state}).then((connectionState) => {
+      console.log("CONNECTION STATE", connectionState);
+      if(connectionState.state === "unauthenticated"){
+        router.push("/login");
+      }else{
+        console.log("SETTING PAGE STATE", connectionState);
+        setPageState(s => ({...s, ...connectionState}));
+        setState(s => ({...s, ...connectionState}));
+      }
+  
+    })
+  }, []);
+  
+  console.log("STATE", JSON.stringify(state));
 
-  return (<h1>
-    <button className="btn">Hello daisyUI</button>
+  return (<>
+  <h1>
+    <button className="btn" onClick={() => {
+      console.log("WAS GEHT DENN SO");
+      router.push("/login");
+    }}>Hello daisyUI updated</button>
+    <button className="btn" onClick={() => {
+      connectionStateAndUserData({state})
+      router.push("/");
+    }}>Reload</button>
     <button className="btn btn-primary">data: {JSON.stringify(state)}</button>
-    </h1>);
+    </h1>
+    <ConnectionBanner state={state} />
+    <h1>{JSON.stringify(state)}</h1>
+    </>);
 }
