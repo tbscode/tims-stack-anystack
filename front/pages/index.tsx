43a1b9ff9cde7ce3678c9c2fac9c794868a284b5
tim <tim@timschupp.de>
2023-05-21 16:38:43 +0200

    adjusted base layout added some colors for marking components

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -17,10 +17,20 @@ export const NOgetServerSideProps = async ({req} : {req: any}) => {
 };
 
 const NavBar = ({children}) => {
+  const navItems = [
+    {id: "about", text: "About", href: "/about"},
+    {id: "settings", text: "Settings", href: "/"},
+    {id: "index", text: "Index", href: "/"},
+  ]
+  /*
+   *           <li><a>Navbar Item 1</a></li>
+          <li><a>Navbar Item 2</a></li>
+  */
+
   return (<div className="drawer">
   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
   <div className="drawer-content flex flex-col">
-    <div className="w-full navbar bg-base-300">
+    <div className="w-auto navbar bg-base-300 mr-3 ml-3 mt-3 rounded-xl">
       <div className="flex-none lg:hidden">
         <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -29,17 +39,19 @@ const NavBar = ({children}) => {
       <div className="flex-1 px-2 mx-2">Navbar Title</div>
       <div className="flex-none hidden lg:block">
         <ul className="menu menu-horizontal">
-          <li><a>Navbar Item 1</a></li>
-          <li><a>Navbar Item 2</a></li>
+            {navItems.map((item) => (
+                <li key={item.id} className='bg-primary rounded-xl ml-1'>
+                  <a href={item.href}>{item.text}</a>
+                </li>
+              ))}
         </ul>
       </div>
     </div>
-    Content
     {children}
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 bg-base-100">
+    <ul className="menu p-4 w-80 bg-base-100 rounded-xl">
       <li><a>Sidebar Item 1</a></li>
       <li><a>Sidebar Item 2</a></li>
       
@@ -47,6 +59,27 @@ const NavBar = ({children}) => {
     
   </div>
 </div>)
+}
+
+export const ChatMessages = ({}) => {
+  return <div className='bg-accent text-accent-content rounded-box flex items-center p-4 shadow-xl'>
+    <div className='flex-1'>Hello</div>
+  </div>
+  
+}
+
+
+export const ChatsList = ({}) => {
+  const chats = [
+    {uuid: "0", name: "Chat 1"}
+  ]
+  return <div></div>
+}
+
+const ProfileSettings = ({}) => {
+ return <div style={{scrollBehavior: 'smooth', scrollPaddingTop: '5rem'}} className='content-center'>
+        <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xl" ></textarea>
+      </div>
 }
 
 
@@ -70,13 +103,8 @@ export default function Index({ pageState, setPageState, updateTheme }): JSX.Ele
   }, []);
   
   console.log("STATE", JSON.stringify(state));
-
-  return (<>
-    <NavBar>
-      <div style={{scrollBehavior: 'smooth', scrollPaddingTop: '5rem'}} className='content-center'>
-        <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xl" ></textarea>
-      </div>
-    </NavBar>
+  /**
+   * 
   <h1>
     <button className="btn" onClick={() => {
       console.log("WAS GEHT DENN SO");
@@ -88,7 +116,22 @@ export default function Index({ pageState, setPageState, updateTheme }): JSX.Ele
     }}>Reload</button>
     <button className="btn btn-primary">data: {JSON.stringify(state)}</button>
     </h1>
+   */
+
+  return (<>
+    <NavBar>
+    <div className="flex flex-row items-center w-auto m-4 h-full">
+      <div className="flex flex-col w-72 bg-primary rounded-xl mr-2 text-wrap h-full p-2">
+        <button className="btn gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+          Button
+        </button>
+      </div>
+      <div className="flex flex-col flex-grow h-auto bg-secondary h-full rounded-xl p-2">
+        <ChatMessages />
+      </div>
+    </div>
+    </NavBar>
     <ConnectionBanner state={state} />
-    <h1>{JSON.stringify(state)}</h1>
     </>);
 }
