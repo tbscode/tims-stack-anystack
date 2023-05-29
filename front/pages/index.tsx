818b9f0cc3692a78726054c703be5f9601ec1f58
tim <tim@timschupp.de>
2023-05-29 15:01:33 +0200

    style adjustments

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -51,7 +51,7 @@ const NavBar = ({children}) => {
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 bg-base-100 rounded-xl">
+    <ul className="menu p-4 w-80 bg-base-100 rounded-xl h-96 mt-20 ml-4">
       <li><a>Sidebar Item 1</a></li>
       <li><a>Sidebar Item 2</a></li>
       
