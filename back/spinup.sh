a345d5184933f2e61c6104a851b11692fec620d6
tim <tim@timschupp.de>
2023-06-07 19:15:08 +0200

    fix single beat redis connection

--- back/spinup.sh
+++ back/spinup.sh
@@ -3,7 +3,7 @@ python3 manage.py migrate
 if [ $PRODUCTION = "true" ]; then
     python3 manage.py shell --command 'from core.tools import get_or_create_base_admin; get_or_create_base_admin()'
     python3 manage.py shell --command 'from core.tools import get_or_create_test_users; get_or_create_test_users()'
+    SINGLE_BEAT_REDIS_SERVER="redis://redis-db-svc.$UNIQUE_ID.svc.cluster.local:6379" single-beat celery -A back beat --loglevel=info &
 fi
 
-SINGLE_BEAT_REDIS_SERVER="redis://redis-db-svc.tims-stack-dev.svc.cluster.local:6379" single-beat celery -A back beat --loglevel=info &
 uvicorn conf.asgi:application --reload --port 8000 --host 0.0.0.0
