e93ebf18aa7e49600debd6c03c69d3c7c7aa1128
tim <tim@timschupp.de>
2023-06-26 16:56:22 +0200

    added getter method for the websocket route in tools

--- front/utils/tools.tsx
+++ front/utils/tools.tsx
@@ -48,9 +48,21 @@ const getServerUrl = () => {
   }
 }
 
+const getWebsocketUrl = () => {
+  if(process.env.NEXT_PUBLIC_LOCAL_MICROK8S === "true"){
+    if(Capacitor.isNativePlatform())
+      return process.env.NEXT_PUBLIC_WS_PATH_ANDROID!
+    return process.env.NEXT_PUBLIC_WS_PATH!
+  }else{
+    if(Capacitor.isNativePlatform())
+      return process.env.NEXT_PUBLIC_WS_PATH_ANDROID!
+    return process.env.NEXT_PUBLIC_WS_PATH!
+  }
+}
+
 export const getEnv = () => {
   const env : Environment = {
-    wsPath: process.env.NEXT_PUBLIC_WS_PATH!,
+    wsPath: getWebsocketUrl(),
     serverUrl: getServerUrl(),
   }
   console.log("ENV", env);
