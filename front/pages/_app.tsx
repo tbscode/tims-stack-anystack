9c2b9c8e991fb313153ff22d651a30def102240a
tim <tim@timschupp.de>
2023-06-26 13:25:07 +0200

    fundamentaly refactored state processing logic, everything is redux managed now

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,39 +1,61 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import { wrapper, store } from "../store/store";
+import { Provider, useSelector } from "react-redux";
+import { ConnectionBanner, connectionStateAndUserData } from '@/components/connection-banner';
+import { MainNavigation } from '@/components/navigation-bar';
+import { useDispatch } from 'react-redux';
+import { DEVICE_STATE } from '@/store/types';
+import { BannerState } from '@/components/connection-banner';
 
 import { useState, useEffect } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  const [stateData, setStateData] = useState({theme: 'dark'});
-  const [width, setWidth] = useState(typeof window === 'undefined' ? 2048 : window.innerWidth);
+const NO_NAVIGATION_ROUTES = ["/login"]
+
+function App({ Component, pageProps }: AppProps) {
+  const deviceState = useSelector((state: any) => state.device)
+  const width = useSelector((state: any) => state.device.width)
+  const frontendSettings = useSelector((state: any) => state.frontendSettings)
+
+  const router = useRouter();
+  const dispatch = useDispatch();
 
   function handleWindowSizeChange() {
-      setWidth(window.innerWidth);
-  
+      const width = window.innerWidth;
+      dispatch({
+        type: DEVICE_STATE,
+        payload: {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          isMobile768: width <= 768,
+          isDesktopLandScape: width > 1024
+        }
+      })
   }
+  
 
   useEffect(() => {
       window.addEventListener('resize', handleWindowSizeChange);
-      setStateData(s => ({...s, ...{device: {isMobile768 : width <= 768, isDesktopLandScape: width > 1024,  width: width, height: window.innerHeight}}}))
       return () => {
           window.removeEventListener('resize', handleWindowSizeChange);
       }
   }, [width]);
 
-  useEffect(() => {
-    setStateData(s => ({...s, ...pageProps, ...{theme: localStorage.getItem('theme') || 'dark'}}))
-  }, [pageProps])
-
-  const updateTheme = (theme) => {
-    console.log("THEME updated", theme)
-    setStateData(s => ({...s, theme: theme}))
-    localStorage.setItem('theme', theme)
-  }
-
   return (
-    <div data-theme={stateData?.theme}>
-      <Component pageState={stateData} setPageState={setStateData} updateTheme={updateTheme} />
-    </div>
+    <Provider store={store}>
+      <div data-theme={frontendSettings?.theme}>
+      {NO_NAVIGATION_ROUTES.includes(router.pathname) ? (
+          <Component/>
+      ): (
+        <MainNavigation>
+          <Component/>
+        </MainNavigation>
+      )}
+        <ConnectionBanner/>
+      </div>
+    </Provider>
   )
 }
 
+export default wrapper.withRedux(App);
