c60cf37fecf58ed30fc3f34cab1d08f8e86339c3
tim <tim@timschupp.de>
2023-06-28 14:55:10 +0200

    tailwind plugin order updated

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -15,5 +15,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
