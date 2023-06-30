8c8cee2d37f75994fcf7a8059fded3c50e19d799
tim <tim@timschupp.de>
2023-06-30 18:16:28 +0200

    included rjsf-daisyui-theme from https://github.com/xtivia/rjsf-tailwind-daisyui-example/tree/main with some updated to the code

---
+++ front/rjsf-daisyui-theme/templates/DescriptionField.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { DescriptionFieldProps } from "@rjsf/utils";
+import { DynamicMarkdown } from "@/components/dynamic-two-page-selector";
+import { useState, useEffect } from "react";
+
+/** The `DescriptionField` is the template to use to render the description of a field
+ *
+ * @param props - The `DescriptionFieldProps` for this component
+ */
+export default function DescriptionField<T = any, F = any>(
+  props: DescriptionFieldProps<T, F>
+) {
+  const { id, description: initDescription } = props;
+  const [description, setDescription] = useState("loading");
+  
+  useEffect(() => {
+    setDescription(initDescription);
+  },[]);
+
+  if (typeof description === "string") {
+    return (
+      <div id={id} className="field-description">
+        <DynamicMarkdown>{description}</DynamicMarkdown>
+      </div>
+    );
+  } else {
+    return (
+      <div id={id} className="field-description">
+        <DynamicMarkdown>{description}</DynamicMarkdown>
+      </div>
+    );
+  }
+}
---
+++ front/rjsf-daisyui-theme/widgets/CheckboxWidget.tsx
@@ -0,0 +1,80 @@
+import React, { useCallback } from "react";
+import { getTemplate, schemaRequiresTrueValue, WidgetProps } from "@rjsf/utils";
+import { DynamicMarkdown } from "@/components/dynamic-two-page-selector";
+
+/** The `CheckBoxWidget` is a widget for rendering boolean properties.
+ *  It is typically used to represent a boolean.
+ *
+ * @param props - The `WidgetProps` for this component
+ */
+function CheckboxWidget<T = any, F = any>({
+  schema,
+  options,
+  id,
+  value,
+  disabled,
+  readonly,
+  label,
+  autofocus = false,
+  onBlur,
+  onFocus,
+  onChange,
+  registry,
+}: WidgetProps<T, F>) {
+  const DescriptionFieldTemplate = getTemplate<
+    "DescriptionFieldTemplate",
+    T,
+    F
+  >("DescriptionFieldTemplate", registry, options);
+  // Because an unchecked checkbox will cause html5 validation to fail, only add
+  // the "required" attribute if the field value must be "true", due to the
+  // "const" or "enum" keywords
+  const required = schemaRequiresTrueValue(schema);
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) =>
+      onChange(event.target.checked),
+    [onChange]
+  );
+
+  const handleBlur = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) =>
+      onBlur(id, event.target.checked),
+    [onBlur, id]
+  );
+
+  const handleFocus = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) =>
+      onFocus(id, event.target.checked),
+    [onFocus, id]
+  );
+
+  return (<>
+      {schema.description && (
+        <DescriptionFieldTemplate
+          id={id + "__description"}
+          description={schema.description}
+          registry={registry}
+        />
+      )}
+    <div className={`checkbox ${disabled || readonly ? "disabled" : ""}`}>
+      <div className="flex flex-row">
+        <input
+          type="checkbox"
+          className="checkbox"
+          id={id}
+          checked={typeof value === "undefined" ? false : value}
+          required={required}
+          disabled={disabled || readonly}
+          autoFocus={autofocus}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+        />
+        <DynamicMarkdown >{label}</DynamicMarkdown>
+      </div>
+    </div>
+  </>);
+}
+
+export default CheckboxWidget;
