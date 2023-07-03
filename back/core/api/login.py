26fe2a97717712fb69e98584dc7bfb8430c92123
tim <tim@timschupp.de>
2023-07-03 22:41:04 +0200

    login api returns the user_data now

--- back/core/api/login.py
+++ back/core/api/login.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes, throttle_classes
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework import status
+from core.api.user_data import get_user_data
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from django.contrib.auth import authenticate, login
 
@@ -40,4 +41,4 @@ def login_user(request):
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
     login(request, user)
-    return Response(status=status.HTTP_200_OK)
+    return Response(get_user_data(user),status=status.HTTP_200_OK)
