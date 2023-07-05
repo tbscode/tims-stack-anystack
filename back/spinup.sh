f3c24a9561e229538e5e66715166b9d72a4533c1
tim <tim@timschupp.de>
2023-07-05 17:53:33 +0200

    registered new global method for generating random base data that includes some chat messages and co

--- back/spinup.sh
+++ back/spinup.sh
@@ -1,6 +1,6 @@
 python3 manage.py migrate
 python3 manage.py shell --command 'from core.tools import get_or_create_base_admin; get_or_create_base_admin()'
-python3 manage.py shell --command 'from core.tools import get_or_create_test_users; get_or_create_test_users()'
+python3 manage.py shell --command 'from core.tools import get_or_create_test_users_and_chats; get_or_create_test_users_and_chats()'
 
 if [ $PRODUCTION = "true" ]; then
     SINGLE_BEAT_REDIS_SERVER="redis://redis-db-svc.$UNIQUE_ID.svc.cluster.local:6379" single-beat celery -A back beat --loglevel=info &
