7019ae080910302cb00234844fe5be35bd310bc3
tim <tim@timschupp.de>
2023-07-05 17:57:32 +0200

    updated user profile viewset

--- back/core/api/profile.py
+++ back/core/api/profile.py
@@ -1,50 +1,23 @@
-from rest_framework import viewsets, status
-from rest_framework import serializers
-from rest_framework import response
+from rest_framework import serializers, viewsets, status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from core.models import UserProfile, UserProfileSerializer
+from rest_framework.pagination import PageNumberPagination
+from core.api.viewsets import UserStaffRestricedModelViewsetMixin
 
-NOT_USER_EDITABLE = ["last_updated"]
 
-def check_unallowed_args(kwargs):
-    res = []
-    for item in NOT_USER_EDITABLE:
-        if item in kwargs:
-            res.append(item)
-    return res
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 8
 
-class UpdateProfileViewset(viewsets.ModelViewSet):
+
+class UpdateProfileViewset(UserStaffRestricedModelViewsetMixin, viewsets.ModelViewSet):
     """
     Simple Viewset for modifying user profiles
     """
+    allow_user_list = False
+    not_user_editable = ["last_updated"]
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
-    
-    def get_object(self):
-        if not self.request.user.is_staff:
-            self.kwargs["pk"] = self.request.user.id
-        else:
-            if not "pk" in self.kwargs:
-                self.kwargs["pk"] = self.request.user.id
-        
-        return super().get_object()
-    
-    def update(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            unallowed_args = check_unallowed_args(request.data)
-            if len(unallowed_args) > 0:
-                return response.Response({arg: "Not User editable" for arg in unallowed_args},status=status.HTTP_400_BAD_REQUEST)
-        return super().update(request, *args, **kwargs)
-    
-    def get_permissions(self):
-        if self.action == 'list':
-            permission_classes = [IsAdminUser]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
-    
-    def get_queryset(self):
-        if not self.request.user.is_staff:
-            return UserProfile.objects.filter(user=self.request.user)
-        else:
-            return UserProfile.objects.all()
+    pagination_class = StandardResultsSetPagination
+    queryset = UserProfile.objects.all()
