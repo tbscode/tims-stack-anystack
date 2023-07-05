6fd884b55647a5a5bafb7fdbeaa8d0acb7c19313
tim <tim@timschupp.de>
2023-07-05 17:51:37 +0200

    updated apis

--- back/core/api/__init__.py
+++ back/core/api/__init__.py
@@ -2,3 +2,5 @@ from .register import register_user
 from .user_data import request_user_data
 from .login import login_user
 from .profile import UpdateProfileViewset
+from .chats import ChatsModelViewSet
+from .messages import MessagesModelViewSet
