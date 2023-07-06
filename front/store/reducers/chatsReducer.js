f2b23de32483da5e46097599a41663544326a1ba
tim <tim@timschupp.de>
2023-07-06 02:15:50 +0200

    simple chats reducer

---
+++ front/store/reducers/chatsReducer.js
@@ -0,0 +1,20 @@
+import { defaultPaginatedObject, CHATS } from '../types'
+
+const initialState = {
+    loading: true,
+    ...defaultPaginatedObject
+}
+
+export default function(state = initialState, action){
+    console.log("action device state", action)
+    switch(action.type){
+        case CHATS:
+        return {
+            ...state,
+            ...action.payload,
+            loading:false
+        }
+        default: return state
+    }
+
+}
