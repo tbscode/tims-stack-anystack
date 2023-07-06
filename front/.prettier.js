bcc78a5abe0a61a6254237a90e3bbfae4bd681be
tim <tim@timschupp.de>
2023-07-06 02:34:32 +0200

    prettyfy prettier

--- front/.prettier.js
+++ front/.prettier.js
@@ -1,9 +1,9 @@
 module.exports = {
-    arrowParens: 'avoid',
-    printWidth: 80,
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: true,
-    trailingComma: 'all',
-  };
+  arrowParens: "avoid",
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: "all",
+};
