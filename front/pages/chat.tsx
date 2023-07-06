62a841cb8c1282395a493566a524819b9a320b42
tim <tim@timschupp.de>
2023-07-06 02:20:46 +0200

    completely integrated backend removed dummy data

--- front/pages/chat.tsx
+++ front/pages/chat.tsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect, useRef, createRef } from "react";
 import { ConnectionBanner, connectionStateAndUserData } from "../components/connection-banner";
 import { MainNavigation, ChatsList, ChatMessages } from "../components/navigation-bar";
 import { useDispatch, useSelector } from 'react-redux';
-import { FRONTEND_SETTINGS } from '../store/types';
+import { FRONTEND_SETTINGS, SLECTED_CHAT } from '../store/types';
 import { DynamicChatSelector, DynamicChat, DynamicTwoPageContentDisplay, NoSelectionPage } from "../components/dynamic-two-page-selector";
 import Form from '@rjsf/core';
 import { RJSFSchema } from '@rjsf/utils';
@@ -17,106 +17,52 @@ import { clear } from 'console';
 import { init } from 'next/dist/compiled/@vercel/og/satori';
 import { DynamicForm } from '@/rjsf-daisyui-theme/form/form';
 
-
-const CHATS = [
-  {
-    id: "some-chat-uuid", 
-    type: "human",
-    partner: {
-      id: "some-partner-uuid",
-      first_name: "John",
-      second_name: "Doe",
-      last_seen: "1 minute ago",
-    },
-  },
-  {
-    id: "some-chat-uuid-2", 
-    type: "human",
-    partner: {
-      id: "some-partner-uuid-2",
-      first_name: "Siobhan",
-      second_name: "Kraus",
-      last_seen: "1 minute ago",
-    },
-  },
-]
-
-const MESSAGES = {
-  "some-chat-uuid" : [
-    {
-      "id": "some-message-uuid",
-      "read": false,
-      "send_time": "",
-      "read_time": "",
-      "sender": "some-sender-uuid",
-      "text": "# Hello World \n \`some code\`",
-    },
-    {
-      "id": "some-message-uuid-2",
-      "read": false,
-      "send_time": "",
-      "read_time": "",
-      "sender": "fea55741-5157-4f86-b854-b4617441a3cd",
-      "text": "Hello World",
-    },
-    {
-      "id": "some-message-uuid-3",
-      "read": false,
-      "send_time": "",
-      "read_time": "",
-      "sender": "some-sender-uuid",
-      "text": "Hello World",
-    },
-    {
-      "id": "some-message-uuid-4",
-      "read": false,
-      "send_time": "",
-      "read_time": "",
-      "sender": "some-sender-uuid",
-      "text": "Hello World",
-    },
-    // Generate 10 messages just in js syntax text right here
-    ...Array(10).fill({
-      "id": "some-message-uuid-5",
-      "read": false,
-      "send_time": "",
-      "read_time": "",
-      "sender": "some-sender-uuid",
-      "text": "# Hello World \n \`some code\`",
-    }),
-]
-}
-
 export default function Chat(): JSX.Element {
-  const [contentFocused, setContentFocused] = useState(false);
   const [selection, setSelection] = useState("empty")
+
+  const dispatch = useDispatch();
+
   const profile = useSelector((state: any) => state.profile);
   const userData = useSelector((state: any) => state.userData);
-  const [selectedChat, setSelectedChat] = useState({});
+  const chats = useSelector((state: any) => state.chats);
+  const allMessagesSel = useSelector((state: any) => state.messages);
+
+  const [selectedChatInfo, setSelectedChatInfo] = useState({});
+  
+  let selectedChat = useSelector((state: any) => state.selectedChat);
+
+  const [contentFocused, setContentFocused] = useState(false);
   const [messages, setMessages] = useState([]);
   const [isChatSelected, setIsChatSelected] = useState(false);
   
-  console.log("SELECTION", selection);
+  useEffect(() => {
+    // TODO: maybe do something if the current selected chat change otherwise ignore
+  }, [allMessagesSel]);
   
+  useEffect(() => {
+    setSelectedChatInfo(selectedChat.chat);
+    setMessages(selectedChat.messages);
+    setIsChatSelected(true);
+  }, [selectedChat]);
+
   useEffect(() => {
     setIsChatSelected(false);
   }, []);
   
-  const updateSelection = (id) => {
-    console.log("UPDATE SELECTION", id, "CONTENT FOCUSED", contentFocused, "SELECTION", selection, "ID", id, "EMPTY", id === "empty")
-    if(id !== "empty") {
-      setSelectedChat(CHATS.filter((item) => item.id === id)[0]);
-      const updatedMessages = id in MESSAGES ? MESSAGES[id] : [];
-      setMessages(updatedMessages);
-      setIsChatSelected(true);
+  const updateSelection = (uuid) => {
+
+    setSelection(uuid); 
+    if(uuid !== "empty") {
+      const updateChatSelection = chats.results.filter((item) => item.uuid === uuid)[0]
+      console.log("TBS SEL MSG", uuid, allMessagesSel[uuid], allMessagesSel)
+      dispatch({type: SLECTED_CHAT, payload: {chat: updateChatSelection, messages: allMessagesSel[uuid]}});
     }    
-    setSelection(id); 
   };
   
   return <DynamicTwoPageContentDisplay 
       focused={contentFocused}
       selectorContent={<DynamicChatSelector
-          sections={CHATS} 
+          sections={chats.results} 
           selection={selection}
           setSelection={updateSelection}
           setContentFocused={setContentFocused}
@@ -124,7 +70,7 @@ export default function Chat(): JSX.Element {
         />}
         navbarMarginContent={false}
         useDynamicSectionHeader={false}
-        sectionHeaderTitle={CHATS.filter((item) => item.id === selection)[0]?.partner.first_name ?? "No Selection"}
+        sectionHeaderTitle={chats.results.filter((item) => item.uuid === selection)[0]?.partner.first_name ?? "No Selection"}
         onSectionHeaderBackClick={() => {
           setContentFocused(false);
           //setSelection("empty");
@@ -134,7 +80,7 @@ export default function Chat(): JSX.Element {
           setSelection("empty");
         }}
         selectionContent={<>
-          <DynamicChat selected={isChatSelected} userData={userData} chat={selectedChat} messages={messages}/>
+          <DynamicChat selected={isChatSelected} userData={userData} chat={selectedChatInfo} messages={messages}/>
           <NoSelectionPage selected={selection === "empty"}/>
         </>}   />
 }
