e6d61de734ee7e07a0c00756ffe409dfcb104f88
tim <tim@timschupp.de>
2023-07-06 02:40:31 +0200

    new user_data api also used when SSR rendering next page through backend now

--- back/core/views.py
+++ back/core/views.py
@@ -1,14 +1,17 @@
 from django.conf import settings
 import requests
 from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes, authentication_classes, throttle_classes
 from core.api.user_data import get_user_data
 from django.shortcuts import redirect
+from rest_framework.request import Request
 
 
+@api_view(['GET'])
 def index(request, path):
     # TODO: use authentication based redirect instead
     if request.user.is_authenticated:
-        return render_nextjs_page(get_user_data(request.user), path=path)
+        return render_nextjs_page(get_user_data(request.user, request), path=path)
     else:
         return render_nextjs_page({"redirect": "/login"}, path=path)
 
