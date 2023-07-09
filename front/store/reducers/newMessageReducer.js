f7d6c7ee9c753068cf19a00b89ad8a000dcfec69
tim <tim@timschupp.de>
2023-07-09 21:15:53 +0200

    updated update order

--- front/store/reducers/newMessageReducer.js
+++ front/store/reducers/newMessageReducer.js
@@ -8,10 +8,11 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case NEW_MESSAGES:
-      const allMessages = state.messages.concat(action.payload.messages);
+      const allMessages = action.payload.messages.concat(state.messages);
       const uniqueNewMessages = Array.from(new Set(allMessages.map(a => a.id))).map(id => {
         return allMessages.find(a => a.id === id)
       });
+      console.log("NEW MESSAGES", uniqueNewMessages);
       return {
         ...state,
         ...action.payload,
