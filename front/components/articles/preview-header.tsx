ecfcd69949cb0e97dd585d289e84b349d30e07c6
tim <tim@timschupp.de>
2023-07-15 17:32:08 +0200

    preview headers now input adjustable

--- front/components/articles/preview-header.tsx
+++ front/components/articles/preview-header.tsx
@@ -1,9 +1,22 @@
+interface ArticleHeaderProps {
+  article: any;  
+  articleHeaderClassName: string;
+  articleHeaderTitleClassName: string;
+  articleHeaderSubitleClassName: string;
+}
 
-export const ArticleHeader = ({ article, articleController }) => {
+export function ArticleHeader({
+    article,
+    articleHeaderClassName = "",
+    articleHeaderTitleClassName = "",
+    articleHeaderSubitleClassName = "",
+}: ArticleHeaderProps) {
   return (
-    <div className="prose w-full bg-base-200 rounded-xl p-1 pl-4 sm:pl-8">
-      <h1 className="mb-0 text-xl sm:text-3xl xl:text-5xl">{article.title}</h1>
-      <h4 className="mt-0">subtitle</h4>
+    <div className={`prose w-full bg-base-200 rounded-xl p-1 pl-4 sm:pl-8 ${articleHeaderClassName}`}>
+      <h1 className={`mb-0 text-xl sm:text-3xl xl:text-5xl ${articleHeaderTitleClassName}`}>
+        {article.title}
+      </h1>
+      <h4 className={`mt-0 ${articleHeaderSubitleClassName}`}>subtitle</h4>
     </div>
   );
-};
+}
